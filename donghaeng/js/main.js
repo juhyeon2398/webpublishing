e7e9901099aefa331034f62bc1dafc643a72db95
wigun <wigun940812@gmail.com>
2020-01-21 18:00:38 +0900

    no message

--- donghaeng/js/main.js
+++ donghaeng/js/main.js
@@ -1,4 +1,4 @@
-new Swiper("#mainVisual",{
+var mainVisual = new Swiper("#mainVisual",{
     seed:1000,
     effect:"slide",
     loop:true,
@@ -8,6 +8,7 @@ new Swiper("#mainVisual",{
     },
     autoplay:{
         delay:10000,
+        // disableOnInteraction:false,
     },
     pagination: {
         el: '#mainVisual .pagination',
@@ -33,3 +34,63 @@ window.addEventListener("scroll",function(){   //스크롤에 따라 속성변�
         header.classList.remove("on");
     }
 })
+
+
+mainVisual.on("slideChange",function(){
+    var idx = mainVisual.realIndex;
+    if(idx === 0){
+        time01.restart();
+    }else if(idx === 1){     
+        time02.restart();      
+    }else if(idx === 2){
+        time03.restart();      
+    }
+});
+
+
+Splitting();
+
+var time01 = gsap.timeline({});
+time01.from("#mainVisual .visual01 .sub",{
+    duration:2,
+    y:-200,
+    opacity:0,
+    ease:Bounce.easeOut,
+    //Power1~4, Elastic,Black, Linear.easeNone,Bounce, easeOut,easeOut
+})
+.from("#mainVisual .visual01 .txtBox .main .char",{
+    duration:1,
+    y:-200,
+    opacity:0,
+    stagger:0.1,     //여러개 옵션은 순서대로 실행
+    ease:Bounce.easeOut,
+},0.5);
+var time02 = gsap.timeline({paused:true});
+time02.from("#mainVisual .visual02 .txtBox .sub",{
+    duration:2,
+    y:-200,
+    opacity:0,
+    ease:Bounce.easeOut,
+})
+.from("#mainVisual .visual02 .txtBox .main .char",{
+    duration:1,
+    y:-200,
+    opacity:0,
+    stagger:0.1, 
+    ease:Bounce.easeOut,
+},0.5);
+var time03 = gsap.timeline({paused:true});
+time03.from("#mainVisual .visual03.swiper-slide-duplicate-prev .txtBox .sub",{
+    duration:2,
+    y:-200,
+    opacity:0,
+    ease:Bounce.easeOut,
+})
+.from("#mainVisual .visual03.swiper-slide-duplicate-prev .txtBox .main .char",{
+    duration:1,
+    y:-200,
+    opacity:0,
+    stagger:0.1,
+    ease:Bounce.easeOut,
+},0.5);
+
--- kai/js/main.js
+++ kai/js/main.js
@@ -1,6 +1,8 @@
+Splitting();
 var gnb=document.querySelectorAll("#gnb .list li");
 var header=document.querySelector("header");
-
+var btnStop=document.querySelector("#mainVisual .btnStop");
+var btnPlay=document.querySelector("#mainVisual .btnPlay");
 for(var i=0;i<gnb.length;i++){
     var item=gnb.item(i);
     item.addEventListener("mouseenter",function(){
@@ -10,17 +12,80 @@ for(var i=0;i<gnb.length;i++){
         header.classList.remove("on");
     })
 }
-new Swiper("#mainVisual",{
+var mainVisual = new Swiper("#mainVisual",{
     seed:1000,
-    effect:"cube",
+    effect:"fade",
     loop:true,
     autoplay:{
-        delay:10000,
+        delay:6000,
+        disableOnInteraction:false,
     },
     pagination: {
-        el: '#mainVisual .pagination',
+        el: '#mainVisual .pagination .bullets',
         type: 'bullets',
-
         clickable:true,
     },
 });
+mainVisual.on("slideChange",function(){
+    var idx = mainVisual.realIndex;
+    if(idx === 0){
+        time01.restart();
+    }else if(idx === 1){     
+        time02.restart();      
+    }else if(idx === 2){
+        time03.restart();      
+    }
+});
+btnStop.addEventListener("click",function(){
+        mainVisual.autoplay.stop();
+        btnPlay.style.display="block";
+        btnStop.style.display="none";
+})
+btnPlay.addEventListener("click",function(){
+        mainVisual.autoplay.start();
+        btnStop.style.display="block";
+        btnPlay.style.display="none";
+})
+var time01 = gsap.timeline({});
+time01.from("#mainVisual .mainVisual01 .txtBox .sub",{
+    duration:2,
+    y:-200,
+    opacity:0,
+    ease:Bounce.easeIn,
+    //Power1~4, Elastic,Black, Linear.easeNone,Bounce, easeIn,easeOut
+})
+.from("#mainVisual .mainVisual01 .txtBox .main .char",{
+    duration:1,
+    y:-200,
+    opacity:0,
+    stagger:0.1,     //여러개 옵션은 순서대로 실행
+    ease:Bounce.easeIn,
+},0.5);
+var time02 = gsap.timeline({paused:true});
+time02.from("#mainVisual .mainVisual02 .txtBox .sub",{
+    duration:2,
+    y:-200,
+    opacity:0,
+    ease:Bounce.easeIn,
+})
+.from("#mainVisual .mainVisual02 .txtBox .main .char",{
+    duration:1,
+    y:-200,
+    opacity:0,
+    stagger:0.1, 
+    ease:Bounce.easeIn,
+},0.5);
+var time03 = gsap.timeline({paused:true});
+time03.from("#mainVisual .mainVisual03 .txtBox .sub",{
+    duration:2,
+    y:-200,
+    opacity:0,
+    ease:Bounce.easeIn,
+})
+.from("#mainVisual .mainVisual03 .txtBox .main .char",{
+    duration:1,
+    y:-200,
+    opacity:0,
+    stagger:0.1,
+    ease:Bounce.easeIn,
+},0.5);
