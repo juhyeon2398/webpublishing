1b766318020525be4fb41f50a93d66d1d505aae4
wigun <wigun940812@gmail.com>
2020-03-13 16:26:25 +0900

    no message

--- my/osomstudio/js/main.js
+++ my/osomstudio/js/main.js
@@ -1,5 +1,4 @@
 $(".order").on("click",function(){
-    console.log("click")
     let order = gsap.timeline();
     order.to("#orderPage",{
         duration:1,
@@ -10,7 +9,6 @@ $(".order").on("click",function(){
     })
 })
 $("#mainVisual #orderPage .close").on("click",function(){
-    console.log("click")
     let close = gsap.timeline();
     close.to("#orderPage",{
         duration:1,
@@ -23,43 +21,60 @@ $("#mainVisual #orderPage .close").on("click",function(){
 
 $(window).on("scroll",function(){
     let st = this.scrollY;
-    console.log("st", st)
-    if(st>300 && st<1700){
-        gsap.to(".section.blue",{
-            duration:1.5,
-            backgroundColor:"#1e19cc",
-        })
+    if(st>300 && st<2000){
+        if($(".section").hasClass("blue")){
+            $(".section.blue").addClass("on");
+            $(".section.blue").css({
+                backgroundColor:"#1e19cc",
+            })
+        }
     }else{
-        gsap.to(".section.blue",{
-            duration:2,
-            backgroundColor:"#fff",
-        })
+        if($(".section").hasClass("blue")){
+            $(".section.blue").removeClass("on");
+            $(".section.blue").css({
+                backgroundColor:"#fff",
+            })
+        }
     }
     if(st>800){
-        $("#header").css({
-            backgroundColor:"#000",
-            color:"#fff"
-        })
-        $("#logo .logoBox .black").css({
-            opacity:0,
-        })
-        $("#logo .logoBox .white").css({
-            opacity:1,
-        })
+        if(!$("#supporter").hasClass("start")){
+            $("#supporter").addClass("start");
+            supporter.restart();
+        }
+        if(!$("#header").hasClass("on")){
+            $("#header").addClass("on");
+            $("#header").css({
+                backgroundColor:"#000",
+                color:"#fff"
+            });
+            $("#logo .logoBox .black").css({
+                opacity:0,
+            })
+            $("#logo .logoBox .white").css({
+                opacity:1,
+            })
+        }
     }
     else{
-        $("#header").css({
-            backgroundColor:"",
-            color:"#333"
-        })
-        $("#logo .logoBox .black").css({
-            opacity:1,
-        })
-        $("#logo .logoBox .white").css({
-            opacity:0,
-        })
+        if($("#supporter").hasClass("start")){
+            $("#supporter").removeClass("start");
+        }
+        if($("#header").hasClass("on")){
+            $("#header").removeClass("on");
+            $("#header").css({
+                backgroundColor:"#fff",
+                color:"#000"
+            });
+            $("#logo .logoBox .black").css({
+                opacity:1,
+            })
+            $("#logo .logoBox .white").css({
+                opacity:0,
+            })
+        }
     }
 })
+
 
 for (let i = 1; i < 16; i++) {
    let logo = `<li><img src="../img/logo${i}.png"></li>`;
@@ -73,7 +88,92 @@ $('.project').tilt({
 $("#mainVisual #nav ul li").on("mouseenter",function(){
     $(this).addClass("rotate");
 })
-
 $("#mainVisual #nav ul li").on("mouseleave",function(){
     $(this).removeClass("rotate");
 })
+let navBar = $("#navBar");
+navBar.on("click",function(){
+    if(!navBar.hasClass("on")){
+        navBar.addClass("on");
+        gsap.to("#mainVisual #nav",{
+            duration:1,
+            rotateY:0,
+            display:"block",
+        })
+    }
+    else{
+        navBar.removeClass("on");
+        gsap.to("#mainVisual #nav",{
+            duration:1,
+            rotateY:90,
+            display:"none",
+        })
+    }
+    
+});
+
+
+
+let mainVisual = gsap.timeline();
+mainVisual.from("#logo",{
+    y:-100,
+    duration:1,
+    ease:"power4.out",
+})
+.from("#mainVisual .titleBox",{
+    y:200,
+    opacity:0,
+    duration:1,
+    ease:"power4.out",
+})
+.from("#gnb ul > li",{
+    y:-100,
+    duration:1,
+    stagger:{
+        each:0.05,
+    },
+    ease:"power4.out",
+})
+.from("#header .order",{
+    y:-100,
+    duration:1,
+    ease:"power4.out",
+})
+.from("#mainVisual video",{
+    y:200,
+    opacity:0,
+    duration:1.5,
+    ease:"power4.out",
+})
+let supporter = gsap.timeline({paused:true});
+supporter.from("#supporter .titleBox strong",{
+    y:100,
+    opacity:0,
+    duration:1,
+    ease:"power4.out",
+})
+.from("#supporter .titleBox span",{
+    y:100,
+    opacity:0,
+    duration:1,
+    ease:"power4.out",
+})
+.from("#supporter .txtBox span",{
+    y:100,
+    opacity:0,
+    duration:1,
+    stagger:{
+        each:0.1,
+    },
+    ease:"power4.out",
+})
+.from("#supporter .logos ul li",{
+    y:100,
+    opacity:0,
+    duration:1,
+    stagger:{
+        each:0.1,
+    },
+    ease:"power4.out",
+})
+
--- novel-works/js/main.js
+++ novel-works/js/main.js
@@ -92,40 +92,174 @@ let mainVisual = new Swiper("#mainVisual .mask",{
     }
 });
 
-let num=0;
-$("#process .icons li").eq(num).addClass("on");
-let iconMove = setInterval(function() { 
-    num++;
-    num = num%8;
-    $("#process .icons li").eq(num).addClass("on");
-    $("#process .icons li").eq(num).siblings().removeClass("on");
- },2000)
 
+ 
 
- let productDesc = new Swiper("#product .descBox .mask",{
+//  product
+let productDesc = new Swiper("#product .descBox .mask",{
     loop:true,
-    speed:1000,
     effect:"fade",
     allowTouchMove:false,
-    fadeEffect:{
-        crossFade:true,
+    speed:1000,
+    fadeEffect: {
+        crossFade: true
     },
     navigation:{
         prevEl:"#product .btnPrev",
         nextEl:"#product .btnNext",
     }
-})
+});
 let product = new Swiper("#product .productBox .mask",{
     loop:true,
-    speed:1000,
-    spaceBetween:200,
     centeredSlides:true,
-    slidesPerView:"auto",
+    breakpoints: {
+        1100: {
+        slidesPerView:"auto",
+        spaceBetween: 250
+        }
+    },
+    slidesPerView:1,
+    spaceBetween:50,
+    //allowTouchMove:false,
+    speed:1000,
     thumbs:{
-        swiper:productDesc,
+        swiper:productDesc
     },
     navigation:{
         prevEl:"#product .btnPrev",
         nextEl:"#product .btnNext",
     }
+});
+
+
+
+// 
+let summaryMotion = gsap.timeline({paused:true});
+summaryMotion.from("#summary .contentsBox:nth-child(1)",{
+    duration:1,
+    opacity:0,
+    x:"-100%",
+    ease:"power4.out"
 })
+.from("#summary .contentsBox:nth-child(2) .contents:nth-child(1) .imgBox",{
+    duration:1,
+    opacity:0,
+    x:"-100%",
+    ease:"power4.out"
+},"-=0.8")
+.from("#summary .contentsBox:nth-child(2) .contents:nth-child(1) .infoBox",{
+    duration:1,
+    opacity:0,
+    x:"-100%",
+    ease:"power4.out"
+},"-=0.8")
+.from("#summary .contentsBox:nth-child(2) .contents:nth-child(2) .infoBox",{
+    duration:1,
+    opacity:0,
+    x:"-100%",
+    ease:"power4.out"
+},"-=0.8")
+.from("#summary .contentsBox:nth-child(2) .contents:nth-child(2) .imgBox",{
+    duration:1,
+    opacity:0,
+    x:"-100%",
+    ease:"power4.out"
+},"-=0.8");
+
+let processMotion = gsap.timeline({paused:true,onComplete:function(){
+    console.log("processMotion end");
+    let num=0;
+    $("#process .icons li").eq(num).addClass("on");
+    let iconMove = setInterval(function() { 
+        num++;
+        num = num%8;
+        $("#process .icons li").eq(num).addClass("on");
+        $("#process .icons li").eq(num).siblings().removeClass("on");
+    },2000);
+}});
+processMotion.from("#process li",{
+    duration:1,
+    opacity:0,
+    x:"-100%",
+    ease:"power4.out",
+    stagger:{
+        each:0.1
+    }
+})
+
+
+
+$(window).on("scroll",function(){
+    let st = $(window).scrollTop();
+    let summary = $("#summary").offset().top;
+    let process = $("#process").offset().top;
+    if(st>summary-300){
+        if(!$("#summary").hasClass("scroll")){
+            $("#summary").addClass("scroll");
+            summaryMotion.restart();
+        }
+    }
+    if(st>process-300){
+        if(!$("#process").hasClass("scroll")){
+            $("#process").addClass("scroll");
+            processMotion.restart();
+        }
+    }
+    if(st>0){
+        if(!$("#header").hasClass("scroll")){
+            $("#header").addClass("scroll");
+        }
+    } else {
+        if($("#header").hasClass("scroll")){
+            $("#header").removeClass("scroll");
+        }
+    }
+})
+
+$(window).on("resize",function() {
+    let w = $(window).width()+17;
+    if(w<=1240) {
+        if(!$("body").hasClass("mobile")){
+            $("body").addClass("mobile");
+        }
+        $("#sitemap").hide();
+        $("body").removeClass("overHidden");
+    } else {
+        if($("body").hasClass("mobile")){
+            $("body").removeClass("mobile");
+            $(".btnAll").removeClass("on");
+            $("#gnb").removeClass("on");
+            $("#gnb .gnbList > li .depth02").removeAttr("style");
+        }
+    }
+})
+$(window).trigger("resize");
+$(window).trigger("scroll");
+
+
+$(".btnAll").on("click",function(){
+    if(!$("body").hasClass("mobile")){
+        $("#sitemap").fadeIn(250);
+        $("body").addClass("overHidden");
+    } else {
+        $("#gnb").toggleClass("on");
+        $(this).toggleClass("on");
+    }
+    return false;
+});
+
+$("#sitemap .btnClose").on("click",function(){
+    $("#sitemap").fadeOut(250);
+    $("body").removeClass("overHidden");
+    return false;
+});
+
+$("#gnb .gnbList > li .depth01").on("click",function(){
+    if($("body").hasClass("mobile")){
+        $(this).next(".depth02").stop().slideToggle();
+        $(this).parent().siblings().find(".depth02").stop().slideUp();
+        $(this).toggleClass("on");
+        $(this).parent().siblings().find(".depth01").removeClass("on");
+        return false;
+    }
+});
